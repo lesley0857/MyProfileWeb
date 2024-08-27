45260844e5532b1cccac06f9ffa9ca695e23565a
lesley <nwekelesley@gmail.com>
2024-08-27 15:13:43 +0100

    first commit

--- devprofileproject/settings.py
+++ devprofileproject/settings.py
@@ -131,4 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
